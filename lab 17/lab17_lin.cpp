dcdb992269502da698801cba7cbc2c13daeffd70
Wilson375695 <Wilson.lin23@login.cuny.edu>
2026-06-17 16:54:45 +0000

    lab 17

--- lab 17/lab17_lin.cpp
+++ lab 17/lab17_lin.cpp
@@ -27,6 +27,28 @@ int main(){
     cout<<"\n --- example 2: 2d array ---"<<endl;
     intro2darray();
 
+    cout<<"\n --- example 3: print 2d array elements ---"<<endl;
+    const int rowsize = 2;
+    int n[][3] = {
+        {1,2,3},
+        {4,5,6}
+    };
+
+    printarray(n, rowsize);
     
+    cout<<"\n --- example 4: 2d array application ---"<<endl;
+    const int students = 3;
+    const int subjects = 4;
+    // declaring 2d array with initial values
+    int grades[students][subjects] ={
+        {85, 90, 78, 92},
+        {70, 88, 84, 76},
+        {95, 91, 89,93}
+    };
+    // declaring 1d array
+    int studentavg[students];
+    studentaverage(grades, students, subjects, studentavg);
+    printavg( studentavg, students);
+
     return 0;
 }
